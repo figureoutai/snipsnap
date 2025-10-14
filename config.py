7de6e4781ea2350ac88d57a7df1d3920650aaad1
Ashish <ashish.averma12@gmail.com>
2025-10-14 15:31:10 +0530

    updated config file

--- config.py
+++ config.py
@@ -1,21 +1,22 @@
+import os
 AWS_REGION = "us-east-1"
 LANGUAGE_CODE="en"
 
-S3_BUCKET_NAME = "clip-highlights-bucket"
+S3_BUCKET_NAME = "highlight-clipping-service-main-975049899047"
 S3_REGION = "us-east-1"
 IMAGE_BUCKET_PREFIX = "images/frame/"
 AUDIO_BUCKET_PREFIX = "audio/streams/"
 
 
-DB_NAME="strangedb"
+DB_NAME=os.environ.get("DB_NAME", "video_processing_db")
 VIDEO_METADATA_TABLE_NAME = "video_metadata"
 AUDIO_METADATA_TABLE_NAME = "audio_metadata"
 SCORE_METADATA_TABLE = "score_metadata"
 STREAM_METADATA_TABLE = "stream_metadata"
 
-DB_HOST = "database-2.cluster-ckdseak4qyg6.us-east-1.rds.amazonaws.com"
+DB_HOST = os.environ.get("DB_URL", "highlight-clipping-service-main-auroracluster-o27b01gfhdja.cluster-ckdseak4qyg6.us-east-1.rds.amazonaws.com")
 DB_PORT = 3306
-DB_SECRET_NAME = "rds!cluster-709bbecc-8432-4da5-8727-aafbf13556e3"
+DB_SECRET_NAME = os.environ.get("SECRET_NAME", "rds!cluster-00500b97-b996-4bb1-9e88-00aef1715034")
 
 # AUDIO CONFIGURATION
 TARGET_SAMPLE_RATE = 16000
